78fe43d22ec0cbb8179f5a14e863c8ccfcde7965
Bscelza11 <bscelza11@gmail.com>
2023-03-23 15:59:14 -0300

    solution challenge routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './features/home/home.component';
 import { LoginComponent } from './features/login/login.component';
 import { ProfileComponent } from './features/profile/profile.component';
 import { RegisterComponent } from './features/register/register.component';
+import { CardDetailComponent } from './features/home/card-detail/card-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
@@ -11,6 +12,8 @@ const routes: Routes = [
   { path: 'sign-up', component: RegisterComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'cards', component: HomeComponent },
+  { path: 'cards/:id', component: CardDetailComponent },
   { path: '**', redirectTo: 'sign-in', pathMatch: 'full' },
 ];
 
